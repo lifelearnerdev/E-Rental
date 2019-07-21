76b80a2614bc21eda0b2200e5460c0eaa2fb45ff
Placide IRANDORA <placideirandora@gmail.com>
2019-07-21 20:39:29 +0200

    feat(sign-up-backend): create a user model

---
+++ server/models/users.js
@@ -0,0 +1,35 @@
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
+  return User;
+};
