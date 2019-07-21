2c0a200813873257450b3f2fab66b58d32397628
Placide IRANDORA <placideirandora@gmail.com>
2019-07-21 14:39:03 +0200

    feat(sign-up-backend): set up the server

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+  res.status(404).json({ status: 404, error: 'route not found' });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ status: 500, error: error.message, next });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+export default app;
