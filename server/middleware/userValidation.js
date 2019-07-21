06a5c3effc420a17f3a02c921b64434b2a2c90f2
Placide IRANDORA <placideirandora@gmail.com>
2019-07-21 22:22:20 +0200

    feat(sign-in-backend): validate user sign in

--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -27,6 +27,29 @@ const validate = {
     }
     next();
   },
+
+  /**
+   * validate user submitted information
+   * prior to a successful user login
+   * @param {object} req
+   * @param {object} res
+   * @param {request} next
+   */
+  validateSignIn(req, res, next) {
+    const loginSchema = Joi.object().keys({
+      email: Joi.string().email({ minDomainAtomas: 2 })
+        .required(),
+      password: Joi.string().alphanum().min(3).max(15)
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, loginSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, '') });
+      return;
+    }
+    next();
+  },
 };
 
 export default validate;
