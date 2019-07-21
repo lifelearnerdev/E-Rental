ce34b0bb4faa50027604caeaf1e65620925cda7f
Placide IRANDORA <placideirandora@gmail.com>
2019-07-21 21:40:24 +0200

    feat(sign-up-backend): decide which database to use according to the current environment

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,20 +1,37 @@
+/* eslint-disable import/no-mutable-exports */
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 import UserModel from '../models/users';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-  },
-  {
-    logging: false,
-  },
-);
+let sequelize;
+
+if (process.env.NODE_ENV === 'test') {
+  sequelize = new Sequelize(
+    process.env.DB_NAME_TEST,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+    {
+      logging: false,
+    },
+  );
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+    {
+      logging: false,
+    },
+  );
+}
 
 const User = UserModel(sequelize, Sequelize);
 
