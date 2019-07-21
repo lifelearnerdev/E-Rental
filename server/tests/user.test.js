892daf1f6e491ef7cd0615fa973fdc27f0d8b8bf
Placide IRANDORA <placideirandora@gmail.com>
2019-07-21 23:34:56 +0200

    feat(sign-in-backend): test the user sign in endpoint

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -2,8 +2,8 @@
 import chai from 'chai';
 import chaiHTTP from 'chai-http';
 import server from '../../app';
-import newUser from './dummy';
 import User from '../config/dbConfig';
+import { newUser, theUser } from './dummy';
 
 chai.use(chaiHTTP);
 chai.should();
@@ -18,6 +18,22 @@ describe('USER ENDPOINT TESTS', () => {
         res.body.should.have.status(201);
         res.body.should.have.property('status').eql(201);
         res.body.should.have.property('success').eql('user registered');
+        res.body.should.be.a('object');
+        res.body.data.should.be.a('array');
+        done();
+      });
+  });
+
+  it('Should sign the new user in', (done) => {
+    chai.request(server)
+      .post('/api/auth/signin')
+      .send(theUser)
+      .set('Accept', 'Application/JSON')
+      .end((err, res) => {
+        res.body.should.have.status(200);
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('success').eql('signed in');
+        res.body.should.have.property('token');
         res.body.should.be.a('object');
         res.body.data.should.be.a('array');
         done();
